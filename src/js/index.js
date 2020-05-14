b3f30904a7953d9dad6108cd5fa990d218451593
Enrico-Orsino Mayer <enrico-orsino.mayer@stud.uni-regensburg.de>
2020-05-14 22:04:05 +0200

    new datastructure + futur skin

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import { GameEngine } from "./GameEngine.js";
-import { SkinChanger } from "../../../03-CSS/src/js/SkinChanger.js";
+import { SkinChanger } from "./SkinChanger.js";
 
 window.gameEngine = new GameEngine(
     document.querySelector(".controls"), 
@@ -8,11 +8,11 @@ window.gameEngine = new GameEngine(
 
 
 let skinStyle = document.querySelector("#skin"),
-    skins = ["gold","peach","basic","win95", "mech"];
+    skins = ["futur", "gold","peach","basic","win95", "mech"];
 
-window.skinChanger = new SkinChanger(skinStyle, skins, "../03-CSS/src/css/");
+window.skinChanger = new SkinChanger(skinStyle, skins, "../css/");
 
 document.querySelector(".next").addEventListener("click", () => skinChanger.next());
 document.querySelector(".previous").addEventListener("click", () => skinChanger.previous());
 
-skinChanger.activeSkin = "peach";
+skinChanger.activeSkin = "futur";
